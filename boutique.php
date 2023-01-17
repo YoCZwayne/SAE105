20fe779623d5aa173cf6f4dd1feb8244ee342bc4
zwayn <ayemenloucif@gmail.com>
2023-01-17 15:46:50 +0100

    test

---
+++ boutique.php
@@ -0,0 +1,30 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-3">
+           <img src="img/speed.jpg">
+        </div>
+        <div class="col-3">
+            <img src="img/speed.jpg">
+        </div>
+        <div class="col-3">
+            <img src="img/speed.jpg">
+        </div>
+        <div class="col-3">
+            <img src="img/speed.jpg">
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
