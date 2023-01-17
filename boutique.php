9aaa145627bf852671c84b74d7d4c1976bbe5f34
zwayn <ayemenloucif@gmail.com>
2023-01-17 16:40:03 +0100

    test

--- boutique.php
+++ boutique.php
@@ -2,7 +2,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
@@ -10,6 +10,48 @@
 <body>
 
 <div class="container-fluid">
+    <div class="row">
+        <div class="col-3">
+           <img src="img/speed.jpg">
+        </div>
+        <div class="col-3">
+            <img src="img/speed.jpg">
+        </div>
+        <div class="col-3">
+            <img src="img/speed.jpg">
+        </div>
+        <div class="col-3">
+            <img src="img/speed.jpg">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+           <img src="img/speed.jpg">
+        </div>
+        <div class="col-3">
+            <img src="img/speed.jpg">
+        </div>
+        <div class="col-3">
+            <img src="img/speed.jpg">
+        </div>
+        <div class="col-3">
+            <img src="img/speed.jpg">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-3">
+           <img src="img/speed.jpg">
+        </div>
+        <div class="col-3">
+            <img src="img/speed.jpg">
+        </div>
+        <div class="col-3">
+            <img src="img/speed.jpg">
+        </div>
+        <div class="col-3">
+            <img src="img/speed.jpg">
+        </div>
+    </div>
     <div class="row">
         <div class="col-3">
            <img src="img/speed.jpg">
